321c96d8f479eee18a67e6c72758222fcb6b8370
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2023-12-03 18:46:00 +0500

    Updated code for API with Node and MongoDB

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// index.js
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -6,6 +8,7 @@ const {
   getAllPlayer,
   deletePlayer,
   updatePlayer,
+  getPlayerById,
 } = require("./operations");
 app.use(express.json());
 
@@ -16,17 +19,68 @@ mongoose
   })
   .then(async () => {
     console.log("Connection to MongoDB created");
-    //let player = await createPlayer("Malik", 95);
-    //console.log(player);
-    //let allPlayer = await getAllPlayer();
-    //console.log(allPlayer);
-    
-    //let updatedPlayer = updatePlayer("65661b01c3cc0048720fe6af","Talha", 150);
-    //console.log(await deletePlayer("5ebb0f03aa9b9b4db09e919e"));
-    let delPlayer = await deletePlayer("65661b01c3cc0048720fe6af");
   })
   .catch((err) => {
     console.log("Error Connecting");
     console.log(err);
   });
-app.listen(3000);
+
+// Create a new player
+app.post("/players", async (req, res) => {
+  try {
+    const { Name, scores } = req.body;
+    const newPlayer = await createPlayer(Name, scores);
+    res.json(newPlayer);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Get all players
+app.get("/players", async (req, res) => {
+  try {
+    const allPlayers = await getAllPlayer();
+    res.json(allPlayers);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Get a player by ID
+app.get("/players/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const player = await getPlayerById(id);
+    res.json(player);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Update a player by ID
+app.put("/players/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { Name, scores } = req.body;
+    const updatedPlayer = await updatePlayer(id, Name, scores);
+    res.json(updatedPlayer);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Delete a player by ID
+app.delete("/players/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedPlayer = await deletePlayer(id);
+    res.json(deletedPlayer);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+const PORT = 3006;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
--- operations.js
+++ operations.js
@@ -1,16 +1,16 @@
 const PlayerModel = require("./models/model");
-const createPlayer = async (Name, score) => {
+const createPlayer = async (Name, scores) => {
   console.log("Create Product");
   let players = new PlayerModel();
   players.Name = Name;
-  players.score = score;
+  players.scores = scores;
   await players.save();
   return players;
 };
-const updatePlayer = async (_id, Name,score) => {
+const updatePlayer = async (_id, Name,scores) => {
   let players = await PlayerModel.findById(_id);
   players.Name = Name;
-  players.score = score;
+  players.scores = scores;
   await players.save();
   return players;
 };
@@ -22,7 +22,12 @@ const deletePlayer = async (_id) => {
   let players = await PlayerModel.findByIdAndDelete(_id);
   return players;
 };
+const getPlayerById = async (_id) => {
+  let players = await PlayerModel.findById(_id);
+  return players;
+};
 module.exports.createPlayer = createPlayer;
 module.exports.getAllPlayer = getAllPlayer;
 module.exports.deletePlayer = deletePlayer;
 module.exports.updatePlayer = updatePlayer;
+module.exports.getPlayerById = getPlayerById;
